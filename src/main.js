807b57b6d5892edbae1ff43f16fd6dea31c54390
Patrik Olofsson <patrik.d.olofsson@gmail.com>
2019-11-10 02:22:44 +0100

    add rateLimit

--- src/main.js
+++ src/main.js
@@ -1,12 +1,18 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const routers = require('routers');
+const rateLimit = require('express-rate-limit');
 
 const port = 8090;
 const app = express();
 
+const limiter = rateLimit({
+  windowMs: 10 * 60 * 1000, // 15 minutes
+  max: 200, // limit each IP to 100 requests per windowMs
+});
 
 app.use(bodyParser.json({ limit: '50mb', extended: true }));
+app.use(limiter);
 routers.setup(app);
 process.on('unhandledRejection', (reason, promise) => {
   console.log('Unhandled Rejection at:  ', 'Reason: ', reason, 'Promise: ', promise);
