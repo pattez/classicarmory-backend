78920cae4ab0e9de2919136d1f759c47d1e2d13f
Patrik Olofsson <patrik.d.olofsson@gmail.com>
2019-11-10 22:07:46 +0100

    removes not working cors

--- src/routers/cors.js
+++ src/routers/cors.js
@@ -6,7 +6,7 @@ router.use((req, res, next) => {
   res.header('Access-Control-Allow-Origin', '*');
   res.header(
     'Access-Control-Allow-Headers',
-    'Content-Type, Authorization, application/json; charset=utf-8;',
+    'Content-Type, Authorization',
   );
   next();
 });
