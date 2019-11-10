676f1ca9d24e30bd5129ea7708c1de2e3bced84b
Patrik Olofsson <patrik.d.olofsson@gmail.com>
2019-11-10 13:52:27 +0100

    adds 8090

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,4 @@
 cp ~/env/db.env .
 cp ~/env/.env .
 sudo docker build -t armory-backend . --no-cache
-sudo docker run --env-file .env -p 80:8090 armory-backend
+sudo docker run --env-file .env -p 8090:8090 armory-backend
