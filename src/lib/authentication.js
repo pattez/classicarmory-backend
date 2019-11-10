b1103459b98a4733062db4309ac95032cc39813f
Patrik Olofsson <patrik.d.olofsson@gmail.com>
2019-11-10 13:56:58 +0100

    adds attributes to uploader fetch

--- src/lib/authentication.js
+++ src/lib/authentication.js
@@ -5,7 +5,14 @@ const PRODUCTION = process.env.NODE_ENV === 'production';
 const authentication = async (req, res, next) => {
   const { authorization } = req.headers;
   if (PRODUCTION) {
-    const user = await models.uploader.findOne({ where: { password: authorization } });
+    const user = await models.uploader.findOne({
+      where: { password: authorization },
+      attributes: [
+        'name',
+        'passowrd',
+        'uploads',
+      ],
+    });
     if (user) {
       const { uploads } = user.dataValues;
       await user.update({ uploads: uploads + 1 });
