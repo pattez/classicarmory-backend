2faca75165cbfae0ba9b58ddef4f7afa5aee76df
Patrik Olofsson <patrik.d.olofsson@gmail.com>
2019-11-16 20:18:14 +0100

    add uploadLimiter

--- src/main.js
+++ src/main.js
@@ -11,8 +11,15 @@ const limiter = rateLimit({
   max: 200, // limit each IP to 100 requests per windowMs
 });
 
+const uploadLimiter = rateLimit({
+  windowMs: 10 * 60 * 1000, // 15 minutes
+  max: 50, // limit each IP to 100 requests per windowMs
+});
+
+
 app.use(bodyParser.json({ limit: '50mb', extended: true }));
 app.use(limiter);
+app.use('/upload', uploadLimiter);
 routers.setup(app);
 process.on('unhandledRejection', (reason, promise) => {
   console.log('Unhandled Rejection at:  ', 'Reason: ', reason, 'Promise: ', promise);
