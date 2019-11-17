73e516d8ce860abae832454f126b39a1c7ab9cb7
Patrik Olofsson <patrik.d.olofsson@gmail.com>
2019-11-17 01:03:25 +0100

    Update honor.js

--- src/routers/honor.js
+++ src/routers/honor.js
@@ -1,26 +1,30 @@
 const express = require('express');
+const { RACES } = require('globals');
 
 const router = express.Router();
-const { getModelNames } = require('helpers/parse');
-const { RACES } = require('globals');
+const Sequelize = require('sequelize');
 const { models } = require('../db');
 
+const { Op } = Sequelize;
+
 router.post('/honor', async (req, res) => {
-  const { serverId, offset } = req.body;
+  const {
+    serverId, offset, faction, orderBy,
+  } = req.body;
+  const races = faction === 1 ? [1, 3, 4, 7] : [2, 5, 6, 8];
   const players = models.player.findAll({
     where: {
       serverId,
+      raceId: races,
+      [`${orderBy}`]: { [Op.not]: null },
     },
-    order: [['todayHonor', 'DESC']],
+    order: [[orderBy, 'DESC']],
     offset: offset || 0,
     limit: 50,
   });
   const result = await players.map(async (player) => {
-    const p = Object.keys(player.dataValues).map(
-      (i) => i.includes('Id') && { model: i.split('Id')[0], id: player.dataValues[i] },
-    ).filter((j) => j.id);
-    const values = await getModelNames(p);
-    return { ...player.dataValues, ...values, faction: RACES[player.dataValues.raceId].faction };
+    const f = RACES[player.dataValues.raceId].faction;
+    return { ...player.dataValues, faction: f === 'Alliance' ? 1 : 2 };
   });
   res.send(result);
 });
